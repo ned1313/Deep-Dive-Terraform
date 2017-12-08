2c9a01bfa0272510f00208c1783d505bcd5b3aef
Ned Bellavance <ned.bellavance@gmail.com>
2017-12-08 04:28:35 -0500

    Mod 5 stuff

---
+++ module5/resources.tf
@@ -0,0 +1,44 @@
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-west-2"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "Terraform"
+
+  cidr = "10.0.0.0/16"
+  azs = "${slice(data.aws_availability_zones.available.names,0,var.subnet_count)}"
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets = ["10.0.0.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = true
+
+  create_database_subnet_group = false
+
+  
+  tags {
+  }
+}
+
+
+
+
+
+
