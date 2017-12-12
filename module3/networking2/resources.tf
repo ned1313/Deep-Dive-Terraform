1b3f335bdf2a0f32ec3e75761e65b83fa858d3ad
Ned Bellavance <ned.bellavance@gmail.com>
2017-12-12 10:02:24 -0500

    adding networking2 folder

--- module3/networking2/resources.tf
+++ module3/networking2/resources.tf
@@ -25,10 +25,10 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
   azs = "${slice(data.aws_availability_zones.available.names,0,var.subnet_count)}"
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets = ["10.0.0.0/24"]
+  private_subnets = ["10.0.1.0/24","10.0.3.0/24"]
+  public_subnets = ["10.0.0.0/24","10.0.2.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
 
   create_database_subnet_group = false
 
