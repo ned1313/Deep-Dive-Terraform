af6cd3f868fe11b4c954d769871c914979a36533
Ned Bellavance <ned.bellavance@gmail.com>
2020-04-23 21:29:11 -0400

    remove interpolation

--- module5/4-networking/resources.tf
+++ module5/4-networking/resources.tf
@@ -4,8 +4,8 @@
 
 provider "aws" {
   version = "~>2.0"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = var.region
 }
 
